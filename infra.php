127211634a43d02b1187c98caed43f80cc85fc0d
Anton Kiyatkin <axlbant@sent.com>
2016-03-05 17:50:28 +0400

    infra

---
+++ infra.php
@@ -0,0 +1,13 @@
+<?php
+namespace infrajs\domready;
+use infrajs\event\Event;
+use infrajs\view\View;
+use infrajs\load\Load;
+
+$domready = false;
+Event::handler('layer.onshow', function ($layer) use (&$domready){
+	if (empty($layer['domready'])) return;
+	if ($domready) return;
+	$domready = true;
+	View::head('<script>'.Load::loadTEXT('-domready/domready.js').'</script>');
+});
