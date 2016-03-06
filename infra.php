46f7db4eaadc0d32929f3162d9e952a931ff8ca3
Антон Кияткин <a.kiyatkin@itlf.ru>
2016-03-06 11:48:27 +0400

    integrate infrajs

--- infra.php
+++ infra.php
@@ -3,11 +3,11 @@ namespace infrajs\domready;
 use infrajs\event\Event;
 use infrajs\view\View;
 use infrajs\load\Load;
-
 $domready = false;
 Event::handler('layer.onshow', function ($layer) use (&$domready){
 	if (empty($layer['domready'])) return;
 	if ($domready) return;
 	$domready = true;
+
 	View::head('<script>'.Load::loadTEXT('-domready/ready.min.js').'</script>');
 });
