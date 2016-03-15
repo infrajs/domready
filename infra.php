e679733575b2474085a0b1a227e3d2d7570fc975
Антон Кияткин < >
2016-03-15 23:27:48 +0400

    fix

--- infra.php
+++ infra.php
@@ -3,11 +3,11 @@ namespace infrajs\domready;
 use infrajs\event\Event;
 use infrajs\view\View;
 use infrajs\load\Load;
+use infrajs\controller\Layer;
 $domready = false;
 Event::handler('layer.onshow', function ($layer) use (&$domready){
-	if (empty($layer['domready'])) return;
 	if ($domready) return;
+	if (!Layer::pop($layer, 'domready')) return;
 	$domready = true;
-
 	View::head('<script>'.Load::loadTEXT('-domready/ready.min.js').'</script>');
 });
