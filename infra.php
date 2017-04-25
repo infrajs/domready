3b0c999bb7654e1d2598e02fd584932c011751bc
Anton <a.kiyatkin@itlf.ru>
2017-04-25 20:18:39 +0400

    fix infra Layer

--- infra.php
+++ infra.php
@@ -5,7 +5,7 @@ use infrajs\view\View;
 use infrajs\load\Load;
 use infrajs\controller\Layer;
 $domready = false;
-Event::handler('layer.onshow', function ($layer) use (&$domready){
+Event::handler('Layer.onshow', function ($layer) use (&$domready){
 	if ($domready) return;
 	if (!Layer::pop($layer, 'domready')) return;
 	$domready = true;
