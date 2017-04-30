b2097f8f59382f36e46bfeaaa5bd8b6db370f90d
Anton <a.kiyatkin@itlf.ru>
2017-05-01 00:29:13 +0400

    fix Once

--- infra.php
+++ infra.php
@@ -3,11 +3,11 @@ namespace infrajs\domready;
 use infrajs\event\Event;
 use infrajs\view\View;
 use infrajs\load\Load;
+use infrajs\once\Once;
 use infrajs\controller\Layer;
-$domready = false;
-Event::handler('Layer.onshow', function ($layer) use (&$domready){
-	if ($domready) return;
+
+Event::handler('Layer.onshow', function ($layer) {
 	if (!Layer::pop($layer, 'domready')) return;
-	$domready = true;
+	if (Once::omit(__FILE__)) return;
 	View::head('<script>'.Load::loadTEXT('-domready/ready.min.js').'</script>');
 });
